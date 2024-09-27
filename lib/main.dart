da6f79e68e301f99ffe195ce150aa3b5bc8d05bf
Sandravijumon <sandravijumon15@gmail.com>
2024-09-27 20:47:00 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+void main()
+{
+  runApp(Patient());
+}
+
+class Patient extends StatelessWidget {
+  const Patient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
